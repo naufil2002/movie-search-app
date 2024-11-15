2ddb591516af43a4d6228853c553870b511d43fd
Naufil Shaikh <141832951+naufil2002@users.noreply.github.com>
2024-11-16 02:26:15 +0530

    final change

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -11,7 +11,7 @@ const MovieCard = ({ movie, onClick }) => {
           display: 'flex',
           flexDirection: 'column',
           height: '530px', 
-        }}
+        }} 
       >
         <img 
           src={movie.Poster} 
